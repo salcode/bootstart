ccc8f23f2f2ae715ecdeb53b651c7956f2289297
Sal Ferrarello <salcode@salogic.net>
2013-09-05 22:59:26 -0400

    tweaks to pagination

--- pagination.php
+++ pagination.php
@@ -8,10 +8,10 @@
 
 <?php
     global $wp_query, $post;
-
-
+    $older = get_next_posts_link('&larr; Older');
+    $newer = get_previous_posts_link('Newer &rarr;');
 ?>
 <ul class="pager">
-  <li class="previous"><a href="#">&larr; Older</a></li>
-  <li class="next disabled"><a href="#">Newer &rarr;</a></li>
+    <li class="previous"><?php echo $older; ?></li>
+    <li class="next"><?php echo $newer; ?></li>
 </ul>
